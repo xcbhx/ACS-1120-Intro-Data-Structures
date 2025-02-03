3885fe2fac3f18e50c012e88d37527c6152ecbe8
Ceina Ellison <ceina.ellison@gmail.com>
2025-02-03 12:01:02 -0500

    completed dictogram.py

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -20,17 +20,34 @@ class Dictogram(dict):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        if word in self:
+            self[word] += count
+        else:
+            self[word] = count
+            self.types += 1
+
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        return self.get(word, 0)
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
-        # TODO: Randomly choose a word based on its frequency in this histogram
+        cumulative_frequency = []
+        total = 0
+        # Creates a cumulative distribution list
+        for word, count in self.items():
+            total += count
+            cumulative_frequency.append((total, word))
 
+        # Pick a random number in the range of total token count 
+        random_value = random.randint(1, total)
+        # Find which word corresponds to the random value
+        for cumulative, word in cumulative_frequency:
+            if random_value <= cumulative:
+                return word
 
 def print_histogram(word_list):
     print()
