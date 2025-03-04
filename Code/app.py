36551cf64938b78978464d2934c57fcf58ebe0a0
Ceina Ellison <ceina.ellison@gmail.com>
2025-03-04 13:38:40 -0500

    fixed render error

--- Code/app.py
+++ Code/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from markov import MarkovChain, read_file
+import os
 
 
 app = Flask(__name__)
 
 # Load the book and create the Markov Chain
-book = read_file("Code/histogram_book.txt")
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "histogram_book.txt")
+book = read_file(file_path)
 markov = MarkovChain(book, order=2) # Create the markov chain.
 
 @app.route("/")
